a103e00e88f1e9c367f1bed4c22712c4030afa42
Jimin Hsieh <jiminbass@gmail.com>
2017-08-15 17:26:26 +0800

    Init commit default sbt version - 0.13.16 and sbt-jmh version - 0.2.27.

---
+++ src/main/g8/src/main/scala/test/TestBenchmark.scala
@@ -0,0 +1,35 @@
+package test
+
+import org.openjdk.jmh.annotations.Benchmark
+
+import scala.io.Source
+
+/**
+  * From [[https://github.com/ktoso/sbt-jmh/blob/master/sbt-jmh-tester/src/main/scala/test/TestBenchmark.scala]]
+  */
+class TestBenchmark {
+  import TestBenchmark._
+
+  @Benchmark
+  def range(): Int =
+    1.to(100000)
+      .filter(_ % 2 == 0)
+      .count(_.toString.length == 4)
+
+  @Benchmark
+  def iterator(): Int =
+    Iterator.from(1)
+      .takeWhile(_ < 100000)
+      .filter(_ % 2 == 0)
+      .count(_.toString.length == 4)
+
+  @Benchmark
+  def readFromFile(): Int =
+    helloFile.getLines()
+      .map(_.length)
+      .sum
+}
+
+object TestBenchmark {
+  val helloFile = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("hello.txt"))
+}
